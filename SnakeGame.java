898a086a989e9cd55c92098b98b0ae907845769b
abh.n.v <abhinavsajeev2004@gmail.com>
2026-02-04 21:27:04 +0530

    JFrame success

---
+++ SnakeGame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class SnakeGame extends JFrame{
+    SnakeGame(){
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new SnakeGame();
+    }
+}
