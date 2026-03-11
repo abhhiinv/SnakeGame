ffe2921fcc1e144a0ff497fd96eead68fac24ee7
abh.n.v <abhinavsajeev2004@gmail.com>
2026-03-11 20:07:27 +0530

    fixed bug on self biting

--- Board.java
+++ Board.java
@@ -3,8 +3,8 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Board extends JPanel implements ActionListener,MouseListener{
-    
+public class Board extends JPanel implements ActionListener, MouseListener {
+
     private Image apple;
     private Image dot;
     private Image head;
@@ -23,7 +23,7 @@ public class Board extends JPanel implements ActionListener,MouseListener{
     private boolean rightDirection = true;
     private boolean upDirection = false;
     private boolean downDirection = false;
-    
+
     private boolean inGame = true;
 
     private int dots;
@@ -34,15 +34,15 @@ public class Board extends JPanel implements ActionListener,MouseListener{
     Board() {
         addKeyListener(new TAdapter());
         addMouseListener(this);
-        setBackground(new Color(168,230,14));//bg color
-        setPreferredSize(new Dimension(300,300));
+        setBackground(new Color(168, 230, 14));//bg color
+        setPreferredSize(new Dimension(300, 300));
         setFocusable(true);
 
         loadImages();
         initGame();
     }
 
-    public void loadImages(){
+    public void loadImages() {
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/apple.png"));
         apple = i1.getImage();
         ImageIcon i2 = new ImageIcon(ClassLoader.getSystemResource("icons/dot.png"));
@@ -50,18 +50,19 @@ public class Board extends JPanel implements ActionListener,MouseListener{
         ImageIcon i3 = new ImageIcon(ClassLoader.getSystemResource("icons/head.png"));
         head = i3.getImage();
     }
-    public void initGame(){
+
+    public void initGame() {
         dots = 3;
         score = 0;
 
-        for(int i=0; i<dots; i++){
-            y[i]=50;
-            x[i]=50 - (i * DOT_SIZE);
+        for (int i = 0; i < dots; i++) {
+            y[i] = 50;
+            x[i] = 50 - (i * DOT_SIZE);
         }
 
         locateApple();
 
-        timer = new Timer(140,this);
+        timer = new Timer(140, this);
         timer.start();
 
         // Reset game state
@@ -73,82 +74,81 @@ public class Board extends JPanel implements ActionListener,MouseListener{
     }
 
     public void locateApple() {
-        int r = (int)(Math.random() * RANDOM_POSITION);
+        int r = (int) (Math.random() * RANDOM_POSITION);
         apple_x = r * DOT_SIZE;
-                
-        r = (int)(Math.random() * RANDOM_POSITION);
+
+        r = (int) (Math.random() * RANDOM_POSITION);
         apple_y = r * DOT_SIZE;
     }
 
-
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         draw(g);
     }
 
-    public void draw(Graphics g){
-        if(inGame){
+    public void draw(Graphics g) {
+        if (inGame) {
 
             g.setColor(Color.BLACK);
             g.setFont(new Font("SAN_SERIF", Font.BOLD, 12));
             g.drawString("Score: " + score, 10, 20);
-            g.drawImage(apple,apple_x,apple_y,this);
-            for (int i=0; i<dots; i++){
-                if(i==0){
+            g.drawImage(apple, apple_x, apple_y, this);
+            for (int i = 0; i < dots; i++) {
+                if (i == 0) {
                     g.drawImage(head, x[i], y[i], this);
-                }else{
+                } else {
                     g.drawImage(dot, x[i], y[i], this);
                 }
             }
 
             Toolkit.getDefaultToolkit().sync();
-        }else{
+        } else {
             gameOver(g);
         }
     }
 
-    public void gameOver(Graphics g){
+    public void gameOver(Graphics g) {
         String msg = "Game Over! Score : " + score;
         Font font = new Font("SAN_SERIF", Font.BOLD, 14);
         FontMetrics metrices = getFontMetrics(font);
-        
+
         g.setColor(Color.BLACK);
         g.setFont(font);
-        g.drawString(msg, (300 - metrices.stringWidth(msg)) / 2, 300/2);
+        g.drawString(msg, (300 - metrices.stringWidth(msg)) / 2, 300 / 2);
 
-         // Draw "Play Again" button
+        // Draw "Play Again" button
         String buttonText = "Play Again";
         Font buttonFont = new Font("SAN_SERIF", Font.BOLD, 12);
         FontMetrics buttonMetrics = getFontMetrics(buttonFont);
-        
+
         int buttonWidth = buttonMetrics.stringWidth(buttonText) + 20;
         int buttonHeight = 30;
         int buttonX = (300 - buttonWidth) / 2;
-        int buttonY = 300/2 + 10;
-        
+        int buttonY = 300 / 2 + 10;
+
         // Store button bounds for click detection
         playAgainButton = new Rectangle(buttonX, buttonY, buttonWidth, buttonHeight);
-        
+
         // Draw button background
         g.setColor(new Color(100, 150, 50));
         g.fillRect(buttonX, buttonY, buttonWidth, buttonHeight);
-        
+
         // Draw button border
         g.setColor(Color.BLACK);
         g.drawRect(buttonX, buttonY, buttonWidth, buttonHeight);
-        
+
         // Draw button text
         g.setFont(buttonFont);
         g.drawString(buttonText, buttonX + 10, buttonY + 20);
     }
 
-    public void move(){
-        for (int i = dots ; i > 0 ; i--) {
+    public void move() {
+        for (int i = dots; i > 0; i--) {
             x[i] = x[i - 1];
             y[i] = y[i - 1];
         }
-        
+
         if (leftDirection) {
             x[0] = x[0] - DOT_SIZE;
         }
@@ -172,12 +172,12 @@ public class Board extends JPanel implements ActionListener,MouseListener{
     }
 
     public void checkCollision() {
-        for(int i = dots; i > 0; i--) {
-            if (( i > 4) && (x[0] == x[i]) && (y[0] == y[i])) {
+        for (int i = dots - 1; i > 3; i--) {
+            if ((x[0] == x[i]) && (y[0] == y[i])) {
                 inGame = false;
             }
         }
-        
+
         if (y[0] >= 300) {
             inGame = false;
         }
@@ -190,7 +190,7 @@ public class Board extends JPanel implements ActionListener,MouseListener{
         if (x[0] < 0) {
             inGame = false;
         }
-        
+
         if (!inGame) {
             timer.stop();
         }
@@ -202,7 +202,7 @@ public class Board extends JPanel implements ActionListener,MouseListener{
             checkCollision();
             move();
         }
-        
+
         repaint();
     }
 
@@ -217,40 +217,45 @@ public class Board extends JPanel implements ActionListener,MouseListener{
     }
 
     @Override
-    public void mousePressed(MouseEvent e) {}
+    public void mousePressed(MouseEvent e) {
+    }
 
     @Override
-    public void mouseReleased(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {
+    }
 
     @Override
-    public void mouseEntered(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {
+    }
 
     @Override
-    public void mouseExited(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {
+    }
 
     public class TAdapter extends KeyAdapter {
+
         @Override
         public void keyPressed(KeyEvent e) {
             int key = e.getKeyCode();
-            
+
             if (key == KeyEvent.VK_LEFT && (!rightDirection)) {
                 leftDirection = true;
                 upDirection = false;
                 downDirection = false;
             }
-            
+
             if (key == KeyEvent.VK_RIGHT && (!leftDirection)) {
                 rightDirection = true;
                 upDirection = false;
                 downDirection = false;
             }
-            
+
             if (key == KeyEvent.VK_UP && (!downDirection)) {
                 upDirection = true;
                 leftDirection = false;
                 rightDirection = false;
             }
-            
+
             if (key == KeyEvent.VK_DOWN && (!upDirection)) {
                 downDirection = true;
                 leftDirection = false;
